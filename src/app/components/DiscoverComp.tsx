b40f34a99db47e789d0b07a17ded39d532ea3765
wiishal <vishalnanaware7850@gmail.com>
2025-11-21 22:46:18 +0530

    modify:ui changes

--- src/app/components/DiscoverComp.tsx
+++ src/app/components/DiscoverComp.tsx
@@ -10,17 +10,8 @@ export default function DiscoverComp() {
   const [warn, setWarn] = useState(true);
 
   return (
-    <div className=" flex flex-col justify-between ">
-      <div className="flex flex-row justify-between w-full">
-        <div className=" text-2xl font-medium">Hey, {user?.firstName} 👋</div>
-      </div>
-      <div className="pt-5">
-        <h1 className="text-2xl font-bold">
-          Are you Looking for Conversation?
-        </h1>
-      </div>
-
-      <div className=" mt-10">
+    <div className=" flex w-full h-full flex-col justify-between ">
+      <div className=" ">
         <RenderBots />
       </div>
       
